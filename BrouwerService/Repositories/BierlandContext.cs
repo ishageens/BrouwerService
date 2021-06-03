8b218150a5ebbd92fe40fc4d19e921276fac3924
Isha <ishageens@msn.com>
2021-06-03 12:05:47 +0200

    Add project files.

---
+++ BrouwerService/Repositories/BierlandContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BrouwerService.Models;
+
+namespace BrouwerService.Repositories
+{
+    public class BierlandContext : DbContext
+    {
+        public BierlandContext(DbContextOptions<BierlandContext> options) : base(options) { }
+        public DbSet<Brouwer> Brouwers { get; set; }
+    }
+}
